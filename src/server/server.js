69ba1fdf10e1751de196961154998ffacaddf4d2
Patrick <patricklee0221@gmail.com>
2021-10-14 13:49:36 +0100

    login request to server

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const SpotifyWebApi = require('spotify-web-api-node');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 app.post('/login', (req, res) => {
   const code = req.body.code;
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: 'http:localhost:3000',
+    redirectUri: 'http://localhost:3000',
     clientId: '9fcb47cd6db644baa7575dcf80494581',
     clientSecret: 'd9c8be53a8bc4d10b5b2aee5cb3367c1',
   });
@@ -21,6 +25,9 @@ app.post('/login', (req, res) => {
       });
     })
     .catch((err) => {
+      console.log(err);
       res.sendStatus(400);
     });
 });
+
+app.listen(3001);
