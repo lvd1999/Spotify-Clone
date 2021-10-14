7e1d09323054e0b97751f71417218206838337e8
Patrick <patricklee0221@gmail.com>
2021-10-14 16:42:42 +0100

    lyrics

--- src/server/server.js
+++ src/server/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const SpotifyWebApi = require('spotify-web-api-node');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const lyricsFinder = require('lyrics-finder');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/refresh', (req, res) => {
   const refreshToken = req.body.refreshToken;
@@ -54,6 +56,13 @@ app.post('/login', (req, res) => {
       console.log(err);
       res.sendStatus(400);
     });
+});
+
+app.get('/lyrics', async (req, res) => {
+  const lyrics =
+    (await lyricsFinder(req.query.artist, req.query.track)) ||
+    'No Lyrics Found';
+  res.json({ lyrics });
 });
 
 app.listen(3001);
