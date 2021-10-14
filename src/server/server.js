72b22b2ac4ebd1b027ab939294a9842eb09ee7cc
Patrick <patricklee0221@gmail.com>
2021-10-14 14:18:07 +0100

    refresh token automatically

--- src/server/server.js
+++ src/server/server.js
@@ -7,6 +7,32 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.post('/refresh', (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:3000',
+    clientId: '9fcb47cd6db644baa7575dcf80494581',
+    clientSecret: 'd9c8be53a8bc4d10b5b2aee5cb3367c1',
+    refreshToken,
+  });
+  // clientId, clientSecret and refreshToken has been set on the api object previous to this call.
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      });
+
+      // Save the access token so that it's used in future calls
+      //   spotifyApi.setAccessToken(data.body['access_token']);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
 app.post('/login', (req, res) => {
   const code = req.body.code;
   const spotifyApi = new SpotifyWebApi({
