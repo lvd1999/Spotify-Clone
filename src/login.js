75209827977f4e5618016fe47d52a1b5cdd1869f
Patrick <patricklee0221@gmail.com>
2021-10-14 12:21:58 +0100

    login components

--- src/login.js
+++ src/login.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import { Container } from 'react-bootstrap';
+
 const AUTH_URL =
-  'https://accounts.spotify.com/authorize?client_id=9fcb47cd6db644baa7575dcf80494581&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-plaback-state';
+  'https://accounts.spotify.com/authorize?client_id=9fcb47cd6db644baa7575dcf80494581&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
 
 export default function login() {
-  return;
-  <div></div>;
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: '100vh' }}
+    >
+      <a className="btn btn-success btn-lg" href={AUTH_URL}>
+        Login with Spotify
+      </a>
+    </Container>
+  );
 }
