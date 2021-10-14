5fdb678d7e93df36bf542b1fda37688d625e7868
Patrick <patricklee0221@gmail.com>
2021-10-14 12:01:25 +0100

    initialize auth url from spotify api

---
+++ src/login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AUTH_URL =
+  'https://accounts.spotify.com/authorize?client_id=9fcb47cd6db644baa7575dcf80494581&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-plaback-state';
+
+export default function login() {
+  return;
+  <div></div>;
+}
